5810028ca69cc96fdd8f45ab499fab7b5b64bc4a
Gabriel Guimaraes <dealmeida.gabriel.d@gmail.com>
2021-02-06 12:43:21 -0300

    Automato verificando se o input faz parte dele.

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         //adicionando 3 estados
         Estado estado1 = new Estado("q1", Boolean.FALSE, Boolean.TRUE);
         Estado estado2 = new Estado("q2");
-        Estado estado3 = new Estado("3", Boolean.TRUE, Boolean.FALSE);
+        Estado estado3 = new Estado("q3", Boolean.TRUE, Boolean.FALSE);
 
         //adicionando transicoes
         Transicao trans1 = new Transicao(estado1, estado2, "1");
@@ -28,12 +28,14 @@ public class Main {
         Transicao trans6 = new Transicao(estado3, estado3, "1");
 
         aut.estados = Arrays.asList(estado1, estado2, estado3);
+        aut.inputs_possiveis = Arrays.asList("1", "2", "3");
         aut.transicoes = Arrays.asList(trans1, trans2, trans3, trans4, trans5, trans6);
         aut.estado_inicial = estado1;
         aut.estados_de_aceitacao = Collections.singletonList(estado3);
 
-        System.out.println(aut.submeteInput(in_errado1, 0, aut.estado_inicial));
-        System.out.println(aut.submeteInput(in_errado2, 0, aut.estado_inicial));
-        System.out.println(aut.submeteInput(in_correto, 0, aut.estado_inicial));
+        System.out.println("É deterministico? " + aut.is_deterministico());
+        System.out.println(aut.pertence_a_linguagem(in_errado1, 0, aut.estado_inicial));
+        System.out.println(aut.pertence_a_linguagem(in_errado2, 0, aut.estado_inicial));
+        System.out.println(aut.pertence_a_linguagem(in_correto, 0, aut.estado_inicial));
     }
 }
--- src/data_shape/Automato.java
+++ src/data_shape/Automato.java
@@ -4,18 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Automato {
     public List<Estado> estados = new ArrayList<>(); //Q
     public List<Transicao> transicoes = new ArrayList<>();
+    public List<String> inputs_possiveis = new ArrayList<>();
     public Estado estado_inicial = new Estado();
     public List<Estado> estados_de_aceitacao = new ArrayList<>(); //Q
 
-    public Boolean submeteInput(List<String> input, Integer index, Estado estado_atual){
+    public Boolean pertence_a_linguagem(List<String> input, Integer index, Estado estado_atual){
         if(index >= input.size()) return estado_atual.de_aceitacao;
+        if(!this.inputs_possiveis.contains(input.get(index))) return Boolean.FALSE;
         Optional<Transicao> opt_trans = this.transicoes.stream()
                 .filter(transicao -> (Objects.equals(estado_atual.nome, transicao.origem.nome)) && (Objects.equals(input.get(index), transicao.valor))).findFirst();
         if(!opt_trans.isPresent()) return Boolean.FALSE;
-        return this.submeteInput(input, (index + 1), opt_trans.get().destino);
+        return this.pertence_a_linguagem(input, (index + 1), opt_trans.get().destino);
+    }
+
+    public Boolean is_deterministico(){
+        return this.estados.stream().allMatch(
+                estado -> this.transicoes.stream().filter(transicao -> (Objects.equals(estado.nome, transicao.origem.nome))).count() == (this.inputs_possiveis.size())
+        );
     }
 }
