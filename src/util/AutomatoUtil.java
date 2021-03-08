4d06fe88a859f834bfb8f85de494a662b33406fc
Gabriel Guimaraes <dealmeida.gabriel.d@gmail.com>
2021-03-08 19:25:47 -0300

    Ajuste na multiplicação.

--- src/util/AutomatoUtil.java
+++ src/util/AutomatoUtil.java
@@ -39,29 +39,6 @@ public class AutomatoUtil {
                 }
             )
         );
-
-//        automato1.inputs_possiveis.forEach(
-//            input -> automato1.estados.forEach(
-//                estado1 -> {
-//                    automato2.estados.forEach(
-//                        estado2 ->{
-//                            Optional<Transicao> opt_trans1 = automato1.transicoes.stream().filter(transicao -> Objects.equals(transicao.origem.id, estado1.id) && Objects.equals(transicao.valor, input)).findFirst();
-//                            Optional<Transicao> opt_trans2 = automato2.transicoes.stream().filter(transicao -> Objects.equals(transicao.origem.id, estado2.id) && Objects.equals(transicao.valor, input)).findFirst();
-//                            if(opt_trans1.isPresent() && opt_trans2.isPresent()){
-//                                Optional<Estado> opt_estado_origem = resultante.estados.stream().filter(estado -> Objects.equals(estado.nome, String.join("_", opt_trans1.get().origem.nome, opt_trans2.get().origem.nome))).findFirst();
-//                                Optional<Estado> opt_estado_destino = resultante.estados.stream().filter(estado -> Objects.equals(estado.nome, String.join("_", opt_trans1.get().destino.nome, opt_trans2.get().destino.nome))).findFirst();
-//                                if(!opt_estado_origem.isPresent() || !opt_estado_destino.isPresent()) return;
-//                                Transicao trans = new Transicao();
-//                                trans.valor = input;
-//                                trans.origem = opt_estado_origem.get();
-//                                trans.destino = opt_estado_destino.get();
-//                                resultado.add(trans);
-//                            }
-//                        }
-//                    );
-//                }
-//            )
-//        );
         return resultado;
     }
 
